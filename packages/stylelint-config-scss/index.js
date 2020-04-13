6032acbdb8464292c0d120cb3071197a75b4e229
Aydin Senturk <hello@aydinsenturk.com>
2020-04-13 08:15:46 +0300

    Stylelint config SCSS updated

--- packages/stylelint-config-scss/index.js
+++ packages/stylelint-config-scss/index.js
@@ -1,13 +1,51 @@
 module.exports = {
   extends: '@startrco/stylelint-config-base',
-  plugins: ['stylelint-scss'],
+  plugins: ['stylelint-order', 'stylelint-scss', 'stylelint-declaration-strict-value'],
   rules: {
-    'at-rule-no-unknown': null,
-    'no-descending-specificity': null,
-    'no-duplicate-selectors': null,
-    'scss/at-rule-no-unknown': true,
-    'scss/dollar-variable-colon-space-after': 'always-single-line',
-    'scss/dollar-variable-colon-space-before': 'never',
+    'max-nesting-depth': [
+      3,
+      {
+        ignoreAtRules: ['each', 'media', 'supports', 'include'],
+      },
+    ],
+    'order/order': [
+      [
+        'custom-properties',
+        'dollar-variables',
+        {
+          type: 'at-rule',
+          name: 'extend',
+        },
+        {
+          type: 'at-rule',
+          name: 'include',
+          hasBlock: false,
+        },
+        'declarations',
+        {
+          type: 'at-rule',
+          name: 'include',
+          hasBlock: true,
+        },
+        'rules',
+      ],
+    ],
+    'scale-unlimited/declaration-strict-value': [
+      ['color', 'z-index'],
+      {
+        ignoreKeywords: 'inherit',
+      },
+    ],
+    'scss/at-extend-no-missing-placeholder': true,
+    'scss/at-function-pattern': '^[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
     'scss/at-import-no-partial-leading-underscore': true,
+    'scss/at-import-partial-extension-blacklist': ['scss'],
+    'scss/at-mixin-pattern': '^[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
+    'scss/at-rule-no-unknown': true,
+    'scss/dollar-variable-colon-space-after': 'always',
+    'scss/dollar-variable-colon-space-before': 'never',
+    'scss/dollar-variable-pattern': '^[_]?[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
+    'scss/percent-placeholder-pattern': '^[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
+    'scss/selector-no-redundant-nesting-selector': true,
   },
 };
